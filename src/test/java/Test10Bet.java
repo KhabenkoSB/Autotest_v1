aca30b307ae055c27d0ccffa60f819dd92f7a197
Maksim Khabenko <maksim.k@sbtech.com>
2017-11-24 09:53:52 +0200

    ff driver

--- src/test/java/Test10Bet.java
+++ src/test/java/Test10Bet.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.annotations.Stories;
 import ru.yandex.qatools.allure.annotations.Title;
@@ -24,13 +25,13 @@ public class Test10Bet {
     @Before
     public void setUp() throws Exception {
 
-        /* for FF
        System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
-        driver = new FirefoxDriver();
-       */
+       driver = new FirefoxDriver();
 
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
+
+
+     //   System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+     //   driver = new ChromeDriver();
         baseUrl = "https://www.10bet.com";
         driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
     }
@@ -40,7 +41,7 @@ public class Test10Bet {
     public void testUntitled() throws Exception {
         String bett;
         String myBett;
-
+        driver.manage().window().maximize();
         driver.get(baseUrl + "/");
         driver.get(baseUrl + "/sports/");
         driver.findElement(By.id("NewHeaderUsername")).click();
