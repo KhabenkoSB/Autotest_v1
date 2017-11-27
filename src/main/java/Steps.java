10d3672f6941304c4890580a129229016173d265
Maksim Khabenko <maksim.k@sbtech.com>
2017-11-27 17:41:15 +0200

    update

--- src/main/java/Steps.java
+++ src/main/java/Steps.java
@@ -1,4 +1,32 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.ru.*;
 
 
 public class Steps {
+    @Given("^I have login$")
+    public void iHaveLogin() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("1");
+    }
+
+    @When("^I am logining to (\\d+)bet$")
+    public void iAmLoginingToBet(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("2");
+    }
+
+    @Then("^I can see balase$")
+    public void iCanSeeBalase() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("3");
+    }
+
+    @Then("^I can see error$")
+    public void iCanSeeError() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("4");
+}
 }
--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/features",
-        glue = "Steps",
+        glue = "",
         tags = "@withdrawal",
         snippets = SnippetType.CAMELCASE
 )
